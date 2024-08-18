1b91d975ec9dd274ab12e1c8e9e60c0cfcae16cf
abecerraguz <alejandrobecerrabecerra73@gmail.com>
2024-08-18 18:29:11 -0400

    Se guardan los cambios

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    let allCard = document.querySelectorAll('.inCategory__add');
+    console.log('Salida de allCard', allCard);
+
+    allCard.forEach( element => {
+
+        let plusIcon = element.querySelector('.bi-plus-lg');
+
+        plusIcon.addEventListener('click', (e) => {
+
+            e.preventDefault();
+
+            element.classList.add('addProduct');
+
+            element.querySelectorAll('.d-none').forEach(child => {
+                child.classList.remove('d-none');
+            });
+
+            setTimeout(() => {
+                element.querySelector('.bi-trash').classList.add('d-none');
+                element.querySelector('.inCategory__number').classList.add('bg-black')
+                plusIcon.classList.add('d-none');
+                element.classList.remove('addProduct');
+            }, 1000 );
+
+        });
+
+
+    });
+    
+});
